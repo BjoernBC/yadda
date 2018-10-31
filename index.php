16d5e47bbc1f8a63212b2453272eff296824af67
Christoffer H. Knudsen <christoffer_knudsen@live.dk>
2018-10-31 17:13:46 +0100

    -redacted-

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require_once('classes/dbh.php');
 $dbCon =  $dbh = DbH::getDbH();
 $pages[] = 'frontpage';
 $page = isset($_GET['page']) ? $_GET['page'] : $pages[0];
-echo "test";
+
 include_once('includes/pages/' . $page . ".php");
 ?>
