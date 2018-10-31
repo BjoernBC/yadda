6e75b2df9bf5826b1e5b437e9ce5c720aa17e5b8
BjoernBC <bbc55755@gmail.com>
2018-10-31 14:28:54 +0100

    Frosted flakes

--- supplementary/yadda.sql
+++ supplementary/yadda.sql
@@ -2,12 +2,12 @@ DROP DATABASE if exists yadda;
 CREATE DATABASE yadda;
 
 CREATE TABLE users(
-	id int(11) PRIMARY KEY NOT NULL,
+	id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	email varchar(255) NOT NULL,
 	name varchar(255) NOT NULL,
 	password BLOB NOT NULL,
 	handle varchar(30) NOT NULL,
-	img varchar(30) NOT NULL,
+	-- img varchar(30) NOT NULL,
 	status boolean NOT NULL DEFAULT 0,
 	permission int(11) NOT NULL
 	);
@@ -19,7 +19,7 @@ CREATE TABLE listeners(
 	);
 
 CREATE TABLE yadda(
-	id int(11) PRIMARY KEY NOT NULL,
+	id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	content varchar(167) NOT NULL,
 	created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	userid int(11) NOT NULL,
@@ -47,8 +47,8 @@ CREATE TABLE image(
 
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS data//
-CREATE PROCEDURE data(email varchar(30), name varchar(30), pwd varchar(30), handle varchar(30), status boolean, permission int(11))
+DROP PROCEDURE IF EXISTS newUser//
+CREATE PROCEDURE newUser(email varchar(30), name varchar(30), pwd varchar(30), handle varchar(30), status boolean, permission int(11))
 BEGIN
 	INSERT INTO users(
 		email,
@@ -60,8 +60,21 @@ BEGIN
 	VALUES (email, name, pwd, handle, status, permission);
 END//
 
+
+DROP PROCEDURE IF EXISTS newYadda//
+CREATE PROCEDURE newYadda(content varchar(167), userid int(11))
+BEGIN
+	INSERT INTO yadda(
+		content,
+		userid)
+	VALUES (content, userid);
+END//
+
 DELIMITER ;
 
-CALL data('admin@mail.dk', 'Admin Adminsen', 'admin', 'admin', 1, 1);
-CALL data('sirup@mail.dk', 'Søren Sirup', 'password', 'SizzleSoren', 0, 0);
-CALL data('sirup@mail.dk', 'Søren Sirup', 'password', 'HotFudge', 1, 0);
+CALL newUser('admin@mail.dk', 'Admin Adminsen', 'admin', 'admin', 1, 1);
+CALL newUser('sirup@mail.dk', 'Søren Sirup', 'password', 'SizzleSoren', 0, 0);
+CALL newUser('mel@mail.dk', 'Morten Melboller', 'password', 'HotFudge', 1, 0);
+
+CALL newYadda('asdfasdfasdfasdfasfkanghbalhbsfafdkaljlkss h aslkhalshoauhffuiahfjjsdkjasdu asddfuhalkfj hasjfn laskdjf,df ,asjj kayejf 
+		bjsd ,adkjb ,k,mx,vkdjhgs,kghjsd v xckgjb rn gjs dfg,s dg sdff ng gfn gµf gsnd fg,sdmn fg', 1);
