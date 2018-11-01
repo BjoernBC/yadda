b3add2d2ab85b005e1f75681eaa544f7070bd43c
TheHasax <TheHasax@10.175.161.12>
2018-11-01 16:07:43 +0100

    Yadda besked boks.

---
+++ includes/pages/Yadda_Containers.php
@@ -0,0 +1,122 @@
+jkindex
+
+<!-- den her skal være for sig -->
+<div class="NewYadda">
+	<div class="PhotoUpload">
+		
+	</div>
+	<div class="InsertText">
+
+	</div>
+</div>
+<!-- den her skal være for sig -->
+
+<!-- Container med forsk. posts starter her -->
+<div class="YaddaContainer"> 
+	<div class="YaddaHeader">
+		
+		<div class="YaddaBruger"> 	
+			
+			<div class="YaddaBrugerImg">
+				
+				<a href="profila.php"> 
+					<img src "dummy"/> 
+				</a>
+
+			</div>
+
+			<div class="YaddaBrugerNavn">
+
+				<a href="profila.php">
+					<h3> 
+						Jens
+					</h3>
+				</a>
+
+			</div>
+
+		</div>
+            <div class="YaddaHeaderRight">
+            <div class="YaddaListenContainer">
+                <label class='YaddaListen'>
+                    <input type="checkbox">
+                    <span class="YaddaListenBtn">
+                        Listen
+                    </span>
+                    <span class="YaddaUnlistenBtn">
+                        Unlisten
+                    </span>
+                </label>
+            </div>
+            
+		<div class="YaddaLike">
+
+			<div class="YaddaRate">
+
+				<div class="YaddaRateUp">
+
+					<a>
+						<img src="dummy"/>
+					</a>
+				
+				</div>
+
+				<div class="YaddaRateDown">
+
+					<a>
+						<img src="dummy"/>
+					</a>
+
+				</div>
+			</div>
+
+		<div class="YaddaRateResult">
+			
+			<p>
+				2k 
+			</p>
+
+		</div>
+
+			<div class="YaddaTagsContainer">
+				
+				<div class="YaddaTags">
+
+					<p>
+						test_fontawesome
+					</p>
+
+				</div>
+
+				<div class="YaddaThread">
+					
+					<p>
+						test_setraad
+					</p>
+
+				</div>				
+			</div>
+            </div>
+		</div>
+        </div>
+	
+	<div class="YaddaMsg">
+
+		<div class="YaddaImg">
+
+			<a>
+				<img src="dummy"/>
+			</a>
+			
+		</div>
+
+		<div class="YaddaTekst">
+
+			<p>
+				test_besked vil placeres her
+			</p>
+			
+		</div>
+	</div>
+</div>
+<!-- Container med forsk. posts slutter her -->
