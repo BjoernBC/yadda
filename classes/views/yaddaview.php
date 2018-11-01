9fa136a92522bb73ad9795ad083e51c448a8428b
BjoernBC <bbc55755@gmail.com>
2018-11-01 23:08:48 +0100

    suppe steg og is

--- classes/views/yaddaview.php
+++ classes/views/yaddaview.php
@@ -8,7 +8,16 @@ class YaddaView extends View {
     }
 
     private function displayAuthor(){
-    	
+    	$str = "<form action='php/actionhandler.php' method='POST'>";
+        $str .= "<input type='hidden' name='action' value='userLogin'>";
+        $str .= "<input type='text' name='email' placeholder='Your email, now!'>";
+        $str .= "<input type='password' name='password' placeholder='and also your password...'>";
+        $str .= "<input type='submit' value='Go HAM'>";
+        $str .= "</form>";
+        if($store){
+            $this->content[] = $str;
+        }
+        return $str;
     }
     private function displayContent(){
     	
